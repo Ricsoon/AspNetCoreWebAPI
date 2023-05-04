b4ce21f17566887e082cb4d11b6b865b7de9aca5
Ricsoon <painonex2@gmail.com>
2023-05-04 19:02:59 -0300

    Preenchendo models com requisitos das classes

--- SmartSchool.API/Models/Professor.cs
+++ SmartSchool.API/Models/Professor.cs
@@ -2,5 +2,14 @@
 {
     public class Professor
     {
+        public Professor(){ }
+        public Professor(int id, string nome)
+        {
+            this.Nome = nome;
+            this.Id = id;
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public IEnumerable<Disciplina> Disciplinas { get; set; }
     }
 }
